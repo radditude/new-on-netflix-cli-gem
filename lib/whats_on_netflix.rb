8f0fd462dce0b5597818c59dcbf10198d3d11d5e
Clare <clare.e.horton@gmail.com>
2016-09-21 22:43:39 +0000

    start stubbing out CLI

--- lib/whats_on_netflix.rb
+++ lib/whats_on_netflix.rb
@@ -2,16 +2,29 @@
 
 module WhatsOnNetflix
     class CLI
-        attr_accessor:input
+        attr_accessor:current_input
         def start
-            puts "Welcome to New On Netflix!"
-            puts "Enter coming-soon for a list of what's coming to Netflix this month."
-            puts "Enter leaving-soon for a list of what's leaving Netflix this month."
-            @input = gets.strip
-            if @input == "coming-soon"
-                puts "1. Princess Diaries"
-                puts "2. "
-            end
+            puts "Welcome to What's On Netflix!"
+            puts "Enter 'today' for today's new Netflix releases," 
+            puts "'coming-soon' for a full list of what's coming to Netflix soon,"
+            puts "or 'leaving-soon' for what to watch before it vanishes."
+            puts "Type 'exit' to quit."
+            @current_input = gets.strip
+                if @current_input == "coming-soon"
+                    puts "1. Princess Diaries"
+                    puts "2. X-Men"
+                    puts "3. Firefly - Season 4"
+                elsif @current_input == "leaving-soon"
+                    puts "1. Nothing"
+                    puts "2. Ever"
+                    puts "3. I miss Doctor Who"
+                elsif @current_input == "today"
+                    puts "1. I'm"
+                    puts "2. Tired"
+                    puts "3. Of making lists"
+                elsif @current_input == "exit"
+                    puts "See you later!"
+                end
         end
     end
 end
