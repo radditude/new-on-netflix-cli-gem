adf9285014af2cf8650e3ed8edf882e7e2a0e196
Clare <clare.e.horton@gmail.com>
2016-09-23 00:01:16 +0000

    move data to separate methods

--- lib/whats_on_netflix.rb
+++ lib/whats_on_netflix.rb
@@ -68,6 +68,82 @@ module WhatsOnNetflix
             end
         end
         
+        def list_leaving_soon
+            puts ""
+            puts "Leaving Soon from Netflix"
+            puts ""
+            puts "1. Nothing (1956)"
+            puts "2. Ever (1843)"
+            puts "3. I miss Doctor Who (2012)"
+            puts ""
+            list_options
+            case
+            when @item == "1"
+                puts ""
+                puts "Nothing (1956) - Documentary - Starring James Franco"
+                puts ""
+                puts "This documentary about James Franco's mind is a critical darling."
+                puts ""
+                item_options
+            when @item == "2"
+                puts ""
+                puts "Ever (1843) - Sci-fi - Starring Willy Wonka"
+                puts ""
+                puts "A older film from before the technical invention of film."
+                puts ""
+                item_options
+            when @item == "3"
+                puts ""
+                puts "I Miss Doctor Who (2016) - Drama - Starring my living room"
+                puts ""
+                puts "I just really wish Doctor Who was still on Netflix."
+                puts ""
+                item_options
+            when @item == "list"
+                @list = "leaving-soon"
+            when @item == "back"
+                @list = ""
+            end
+        end
+        
+        def list_today
+            puts ""
+            puts "Added Today on Netflix"
+            puts ""
+            puts "1. I'm (1988)"
+            puts "2. Tired (2016)"
+            puts "3. Of making lists - Season 5 (2016)"
+            puts ""
+            list_options
+            case
+            when @item == "1"
+                puts ""
+                puts "I'm (1988) - Documentary - Starring self"
+                puts ""
+                puts "This is a fake movie because I'm tired of making up plot summaries."
+                puts ""
+                item_options
+            when @item == "2"
+                puts ""
+                puts "Tired (2016) - True Crime - starring my bed"
+                puts ""
+                puts "Seriously ready to get on with the interesting part here."
+                puts ""
+                item_options
+            when @item == "3"
+                puts ""
+                puts "Of Making Lists (2011) - TV Series - Starring Li Ul, Li Ol"
+                puts ""
+                puts "This is the last item on my list!"
+                puts ""
+                item_options
+            when @item == "list"
+                @list = "today"
+            when @item == "back"
+                @list = ""
+            end
+        end
+        
         def start
             while @list != "exit" && @item != "exit"
                 list_available_commands
@@ -76,78 +152,10 @@ module WhatsOnNetflix
                     list_coming_soon
                 end
                 while @list == "leaving-soon"
-                    puts ""
-                    puts "Leaving Soon from Netflix"
-                    puts ""
-                    puts "1. Nothing (1956)"
-                    puts "2. Ever (1843)"
-                    puts "3. I miss Doctor Who (2012)"
-                    puts ""
-                    list_options
-                    case
-                    when @item == "1"
-                        puts ""
-                        puts "Nothing (1956) - Documentary - Starring James Franco"
-                        puts ""
-                        puts "This documentary about James Franco's mind is a critical darling."
-                        puts ""
-                        item_options
-                    when @item == "2"
-                        puts ""
-                        puts "Ever (1843) - Sci-fi - Starring Willy Wonka"
-                        puts ""
-                        puts "A older film from before the technical invention of film."
-                        puts ""
-                        item_options
-                    when @item == "3"
-                        puts ""
-                        puts "I Miss Doctor Who (2016) - Drama - Starring my living room"
-                        puts ""
-                        puts "I just really wish Doctor Who was still on Netflix."
-                        puts ""
-                        item_options
-                    when @item == "list"
-                        @list = "leaving-soon"
-                    when @item == "back"
-                        @list = ""
-                    end
+                    list_leaving_soon
                 end
                 while @list == "today"
-                    puts ""
-                    puts "Added Today on Netflix"
-                    puts ""
-                    puts "1. I'm (1988)"
-                    puts "2. Tired (2016)"
-                    puts "3. Of making lists - Season 5 (2016)"
-                    puts ""
-                    list_options
-                    case
-                    when @item == "1"
-                        puts ""
-                        puts "I'm (1988) - Documentary - Starring self"
-                        puts ""
-                        puts "This is a fake movie because I'm tired of making up plot summaries."
-                        puts ""
-                        item_options
-                    when @item == "2"
-                        puts ""
-                        puts "Tired (2016) - True Crime - starring my bed"
-                        puts ""
-                        puts "Seriously ready to get on with the interesting part here."
-                        puts ""
-                        item_options
-                    when @item == "3"
-                        puts ""
-                        puts "Of Making Lists (2011) - TV Series - Starring Li Ul, Li Ol"
-                        puts ""
-                        puts "This is the last item on my list!"
-                        puts ""
-                        item_options
-                    when @item == "list"
-                        @list = "today"
-                    when @item == "back"
-                        @list = ""
-                    end
+                    list_today
                 end
             end
             puts "See you later!"
