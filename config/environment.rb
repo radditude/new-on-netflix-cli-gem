7d065bc1cef3a9b05280b23dd4f04eb03af76b4a
Clare <clare.e.horton@gmail.com>
2016-09-23 03:41:11 +0000

    reconfigure cli logic to use recursion instead of loops

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,5 @@ require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
-require_relative '../lib/whats_on_netflix/lister'
+require_relative '../lib/whats_on_netflix/movie'
+require_relative '../lib/whats_on_netflix/scraper'
