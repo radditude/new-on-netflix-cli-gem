2f05f20d75b72979454722918ce11fc12e1017ed
Clare <clare.e.horton@gmail.com>
2016-09-24 05:51:12 +0000

    building #coming_soon_from_list

--- lib/whats_on_netflix/movie.rb
+++ lib/whats_on_netflix/movie.rb
@@ -4,6 +4,17 @@ module WhatsOnNetflix
         @@coming_soon = []
         @@leaving_soon = []
         
+        @@comingsoontitles = ["Princess Diaries (2016)", "X-Men (1999)", "Firefly - Season 2 (2001)"]
+        @@comingsoondata = [
+            [:title => "Princess Diaries", :year =>"2001", :genre => "Comedy", :stars => "Anne Hathaway, Julie Andrews", :plot => "This girl is a princess and she does stuff and Julie Andrews is a boss."],
+            [:title => "X-Men", :year =>"1999", :genre => "Sci-fi", :stars => "Hugh Jackman, Anna Paquin", :plot => "Mutants doing mutant stuff. Ian McKellan is in this movie."],
+            [:title => "Firefly", :year =>"2001", :genre => "Sci-fi", :stars => "Gina Rodriguez, Alan Tudyk", :plot => "Sci-fi cult show by that guy who does all the good sci-fi shows."]
+            ]
+            
+        def initialize(title)
+            @title = title
+        end
+        
         def self.coming_soon
             @@coming_soon
         end
@@ -12,44 +23,59 @@ module WhatsOnNetflix
             @@leaving_soon
         end
         
+        def self.comingsoontitles
+            @@comingsoontitles
+        end
+        
         def self.add_movies
-            self.movie_1
-            self.movie_2
-            self.movie_3
+            self.coming_soon_from_list(@@comingsoontitles)
             self.movie_4
             self.movie_5
             self.movie_6
         end
         
-        def self.movie_1
-            movie = self.new
-            movie.title = "Princess Diaries"
-            movie.year = "2001"
-            movie.genre = "comedy"
-            movie.stars = "Anne Hathaway, Julie Andrews"
-            movie.plot = "This girl is a princess and she does stuff and Julie Andrews is a boss."
-            @@coming_soon << movie
+        def self.coming_soon_from_list(array)
+            array.each do |entry|
+                title = ""
+                if entry.include?("-")
+                    title = entry.delete!("\-.+")
+                else
+                    title = entry.delete("/\([0-9]+\)/")
+                end
+                movie = self.new(title.strip)
+                @@coming_soon << movie
+            end
         end
         
-        def self.movie_2
-            movie = self.new
-            movie.title = "X-Men"
-            movie.year = "1999"
-            movie.genre = "Sci-fi"
-            movie.stars = "Hugh Jackman, Anna Paquin"
-            movie.plot = "Mutants doing mutant stuff. Ian McKellan is in this movie."
-            @@coming_soon << movie
-        end
+    #    def self.movie_1
+    #        movie = self.new
+    #        movie.title = "Princess Diaries"
+    #        movie.year = "2001"
+    #        movie.genre = "comedy"
+    #        movie.stars = "Anne Hathaway, Julie Andrews"
+    #        movie.plot = "This girl is a princess and she does stuff and Julie Andrews is a boss."
+    #        @@coming_soon << movie
+    #    end
         
-        def self.movie_3
-            movie = self.new
-            movie.title = "Firefly"
-            movie.year = "2001"
-            movie.genre = "Sci-fi"
-            movie.stars = "Gina Rodriguez, Alan Tudyk"
-            movie.plot = "Sci-fi cult show by that guy who does all the good sci-fi shows."
-            @@coming_soon << movie
-        end
+    #    def self.movie_2
+    #        movie = self.new
+    #        movie.title = "X-Men"
+    #        movie.year = "1999"
+    #        movie.genre = "Sci-fi"
+    #        movie.stars = "Hugh Jackman, Anna Paquin"
+    #        movie.plot = "Mutants doing mutant stuff. Ian McKellan is in this movie."
+    #        @@coming_soon << movie
+    #    end
+        
+    #    def self.movie_3
+    #        movie = self.new
+    #        movie.title = "Firefly"
+    #        movie.year = "2001"
+    #        movie.genre = "Sci-fi"
+    #        movie.stars = "Gina Rodriguez, Alan Tudyk"
+    #        movie.plot = "Sci-fi cult show by that guy who does all the good sci-fi shows."
+    #        @@coming_soon << movie
+    #    end
         
         def self.movie_4
             movie = self.new
