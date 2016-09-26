64d28ca6c884783621ae880c5c9f5b39bda30495
Clare <clare.e.horton@gmail.com>
2016-09-26 05:31:40 +0000

    title list scraper works

--- lib/whats_on_netflix/coming_soon.rb
+++ lib/whats_on_netflix/coming_soon.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 module WhatsOnNetflix
     class ComingSoon
         attr_accessor:display_title, :title, :year, :genre, :stars, :plot
@@ -20,19 +22,14 @@ module WhatsOnNetflix
         end
         
         def self.add_movies
-            self.coming_soon_from_list(COMINGSOONTITLES)
+            self.coming_soon_from_list(WhatsOnNetflix::Scraper.scrape_title_list("http://www.whats-on-netflix.com/coming-soon/september-2016-new-netflix-releases/"))
         end
         
         def self.coming_soon_from_list(array)
             array.each do |entry|
                 movie = self.new(entry)
-                if entry.include?(" - ")
-                    new_title = entry.split(" - ")
-                    movie.title = new_title[0]
-                else
-                    new_title = entry.split(" (")
-                    movie.title = new_title[0]
-                end
+                new_title = entry.split("(")
+                movie.title = new_title[0].strip
                 @@all << movie
             end
         end
@@ -44,49 +41,6 @@ module WhatsOnNetflix
             movie.stars = hash[:stars]
             movie.plot = hash[:plot]
         end
-        
-    #    def self.clean_title(title)
-    #        movie = self.new(title)
-    #        if entry.include?(" - ")
-    #            new_title = entry.split(" - ")
-    #            movie.title = new_title[0]
-    #        else
-    #            new_title = entry.split(" (")
-    #            movie.title = new_title[0]
-    #        end
-    #    end
-        
-    #    def self.movie_1
-    #        movie = self.new
-    #        movie.title = "Princess Diaries"
-    #        movie.year = "2001"
-    #        movie.genre = "comedy"
-    #        movie.stars = "Anne Hathaway, Julie Andrews"
-    #        movie.plot = "This girl is a princess and she does stuff and Julie Andrews is a boss."
-    #        @@coming_soon << movie
-    #    end
-        
-    #    def self.movie_2
-    #        movie = self.new
-    #        movie.title = "X-Men"
-    #        movie.year = "1999"
-    #        movie.genre = "Sci-fi"
-    #        movie.stars = "Hugh Jackman, Anna Paquin"
-    #        movie.plot = "Mutants doing mutant stuff. Ian McKellan is in this movie."
-    #        @@coming_soon << movie
-    #    end
-        
-    #    def self.movie_3
-    #        movie = self.new
-    #        movie.title = "Firefly"
-    #        movie.year = "2001"
-    #        movie.genre = "Sci-fi"
-    #        movie.stars = "Gina Rodriguez, Alan Tudyk"
-    #        movie.plot = "Sci-fi cult show by that guy who does all the good sci-fi shows."
-    #        @@coming_soon << movie
-    #    end
-        
-        
         
     end
 end
