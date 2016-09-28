07ae01fc3202e59328ed671c610464902d893b70
Clare <clare.e.horton@gmail.com>
2016-09-28 05:15:21 +0000

    add basic spec coverage

--- spec/01_whats_on_netflix_spec.rb
+++ spec/01_whats_on_netflix_spec.rb
@@ -1,7 +1,34 @@
 require "spec_helper"
+require 'stringio'
 
 describe WhatsOnNetflix do
+  
+  #disables long lists of output
+  before do
+    $stdout = StringIO.new
+  end
+  after(:all) do
+    $stdout = STDOUT
+  end
+  
   it "has a version number" do
     expect(WhatsOnNetflix::VERSION).not_to be nil
+  end
+  
+  it "lists Coming Soon" do
+    WhatsOnNetflix::CLI.new
+    expect(WhatsOnNetflix::ComingSoon.list).not_to be nil
+  end
+  
+  it "lists Coming Soon movie info" do
+    expect(WhatsOnNetflix::ComingSoon.item("1")).not_to be nil
+  end
+  
+  it "lists Leaving Soon" do
+    expect(WhatsOnNetflix::LeavingSoon.list).not_to be nil
+  end
+  
+  it "lists Leaving Soon movie info" do
+    expect(WhatsOnNetflix::LeavingSoon.item("1")).not_to be nil
   end
 end
--- spec/02_lister_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe WhatsOnNetflix::OneClass do
-    it 'greets the user' do
-        expect(STDOUT).to receive(:puts).with('howdy')
-        WhatsOnNetflix::OneClass.new.greetings
-    end
-end
