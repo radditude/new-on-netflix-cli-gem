b46af05036fdf417a5166cb72f817020ab75cd70
CJ Horton <thecjhorton@gmail.com>
2017-05-05 20:35:44 -0500

    fixed whats-on-netflix.com url error

--- lib/whats_on_netflix/coming_soon.rb
+++ lib/whats_on_netflix/coming_soon.rb
@@ -2,17 +2,17 @@ require 'pry'
 
 module WhatsOnNetflix
     class ComingSoon < List
-        
+
         @@all = []
-        
+
         def self.all
             @@all
         end
-        
+
         def self.list_url
-            "http://www.whats-on-netflix.com/coming-soon/#{self.current_month}-#{self.current_year}-new-netflix-releases/"
+            "https://www.whats-on-netflix.com/coming-soon/#{self.current_month}-#{self.current_year}-new-netflix-releases/"
         end
-        
+
         def self.list
             puts "= = = = ="
             puts "Coming Soon on Netflix"
--- lib/whats_on_netflix/leaving_soon.rb
+++ lib/whats_on_netflix/leaving_soon.rb
@@ -1,15 +1,15 @@
 module WhatsOnNetflix
     class LeavingSoon < List
        @@all = []
-        
+
         def self.all
             @@all
         end
-        
+
         def self.list_url
-            "http://www.whats-on-netflix.com/leaving-soon/titles-leaving-netflix-in-#{self.current_month}-#{self.current_year}/"
+            "https://www.whats-on-netflix.com/leaving-soon/titles-leaving-netflix-in-#{self.current_month}-#{self.current_year}/"
         end
-        
+
         def self.list
             puts "= = = = ="
             puts "Leaving Soon from Netflix"
